cf1bbd7c6a7f96de9a12f03f537bfae6ce32bc6e
JinZeping <40210080+JinZeping@users.noreply.github.com>
2021-01-13 21:53:09 +0800

    修复gitignore错误

---
+++ StockApp/StockApp/DB/Models/SeasonFinancialBrief.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StockApp.DB.Models
+{
+    public class SeasonFinancialBrief
+    {
+        public int ID { get; set; }
+
+        [Required, Column(TypeName = "date")]
+        public DateTime Date { get; set; }
+
+        #region 利润表摘要
+
+        /// <summary>
+        /// 营业收入（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? OperatingIncome { get; set; }
+
+        /// <summary>
+        /// 营业成本（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? OperatingCosts { get; set; }
+
+        /// <summary>
+        /// 营业利润（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? OperatingProfit { get; set; }
+
+        /// <summary>
+        /// 利润总额（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? TotalProfit { get; set; }
+
+        /// <summary>
+        /// 所得税费用（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? IncomeTax { get; set; }
+
+        /// <summary>
+        /// 净利润（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? NetProfit { get; set; }
+
+        /// <summary>
+        /// 基本每股收益
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? BasicEarningPerShare { get; set; }
+
+        #endregion
+
+        #region 资产负债表摘要
+
+        /// <summary>
+        /// 货币资金（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? Money { get; set; }
+
+        /// <summary>
+        /// 应收账款（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? Receivables { get; set; }
+
+        /// <summary>
+        /// 存货（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? Inventory { get; set; }
+
+        /// <summary>
+        /// 流动资产合计（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? TotalCurrentAssets { get; set; }
+
+        /// <summary>
+        /// 固定资产净额（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? NetFixedAssets { get; set; }
+
+        /// <summary>
+        /// 资产总计（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? TotalAssets { get; set; }
+
+        /// <summary>
+        /// 流动负债合计（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? TotalCurrentLiabilities { get; set; }
+        /// <summary>
+        /// 非流动负债合计（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? TotalNonCurrentLiabilities { get; set; }
+
+        /// <summary>
+        /// 负债合计（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? TotalLiabilities { get; set; }
+
+        /// <summary>
+        /// 所有者权益（股东权益）合计（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? TotalOwnerEquity { get; set; }
+
+        #endregion
+
+        #region 现金流量表摘要
+
+        /// <summary>
+        /// 期初现金及现金等价物余额（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? BeginBalanceOfCashAndCashEquivalents { get; set; }
+
+        /// <summary>
+        /// 期末现金及现金等价物余额（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? EndBalanceOfCashAndCashEquivalents { get; set; }
+
+        /// <summary>
+        /// 经营活动产生的现金流量净额（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? NetCashFlowFromOperating { get; set; }
+
+        /// <summary>
+        /// 投资活动产生的现金流量净额（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? NetCashFlowFromInvestment { get; set; }
+
+        /// <summary>
+        /// 筹资活动产生的现金流量净额（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? NetCashFlowFromFinancing { get; set; }
+
+        /// <summary>
+        /// 现金及现金等价物净增加额（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? NetIncreaseInCashAndCashEquivalents { get; set; }
+
+        #endregion
+
+        [Required]
+        public int StockID { get; set; }
+        public Stock Stock { get; set; }
+    }
+}
---
+++ StockApp/StockApp/DB/Models/SeasonFinancialSummary.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StockApp.DB.Models
+{
+    /// <summary>
+    /// 季度财务报告概要
+    /// </summary>
+    public class SeasonFinancialSummary
+    {
+        public int ID { get; set; }
+
+        [Required, Column(TypeName = "date")]
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// 基本每股收益（元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? PrimaryEarningsPerShare { get; set; }
+
+        /// <summary>
+        /// 每股净资产（元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2")]
+        public decimal? NetAssetsPerShare { get; set; }
+
+        /// <summary>
+        /// 每股经营活动现金净流量额（元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2")]
+        public decimal? OperatingNetCashFlowPerShare { get; set; }
+
+        /// <summary>
+        /// 主营业务收入（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? MainBusinessIncome { get; set; }
+
+        /// <summary>
+        /// 主营业务利润（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? MainBusinessProfit { get; set; }
+
+        /// <summary>
+        /// 营业利润（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? BusinessProfit { get; set; }
+
+        /// <summary>
+        /// 投资收益（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? InvestmentReturn { get; set; }
+
+        /// <summary>
+        /// 营业外收支净额（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? NetNonOperatingIncome { get; set; }
+
+        /// <summary>
+        /// 利润总额（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2")]
+        public decimal? TotalProfit { get; set; }
+
+        /// <summary>
+        /// 净利润（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2")]
+        public decimal? NetProfit { get; set; }
+
+        /// <summary>
+        /// 净利润（扣除非经常性损益后）（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2")]
+        public decimal? NetProfitAfterDeductingNonRecurring { get; set; }
+
+        /// <summary>
+        /// 经营活动产生的现金流量净额（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2")]
+        public decimal? NetCashFlowFromOperatingActivities { get; set; }
+
+        /// <summary>
+        /// 现金及现金等价物净增加额（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2")]
+        public decimal? NetIncreaseInCashAndEquivalents { get; set; }
+
+        /// <summary>
+        /// 总资产（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2")]
+        public decimal? TotalAssets { get; set; }
+
+        /// <summary>
+        /// 流动资产（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2")]
+        public decimal? CurrentAssets { get; set; }
+
+        /// <summary>
+        /// 总负债（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2")]
+        public decimal? TotalLiabilities { get; set; }
+
+        /// <summary>
+        /// 流动负债（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2")]
+        public decimal? CurrentLiabilities { get; set; }
+
+        /// <summary>
+        /// 所有者权益/股东权益不含少数股东权益（万元）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? OwnerEquity { get; set; }
+
+        /// <summary>
+        /// 净资产收益率加权（%）
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal? WeightedReturnOnEquity { get; set; }
+
+        [Required]
+        public int StockID { get; set; }
+        public Stock Stock { get; set; }
+    }
+}
